27fbd3046646996a11097d889ddb430871fc029a
Lachezar Kunov <lachezarkunovbg@gmail.com>
2024-10-15 23:46:35 +0300

    Add custom styles and implement home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import './HomePage.css';
 
 function HomePage () {
     return (
         <>
-            <h5>
-            Hi there!<br></br> 
-            I’m Lachezar Kunov, a third-year software engineering student. <br></br>
-             Welcome to my portfolio!
-            </h5>
+        <div className="homepage-container">
+            <div className="text-container">
+                <h1>Welcome.</h1>
+                <p>
+                I’m Lachezar Kunov,<br></br> 
+                a third-year student in Information Systems.<br></br> 
+                As a motivated software engineer,<br></br>
+                I am eager to enhance my skills in software development and contribute to innovative projects.
+                </p>
+            </div>
+            <div className="img-container">
+                <img src="../../public/assets/software-engineer.png" alt="" />
+            </div>
+        </div>
         </>
     )
 }
